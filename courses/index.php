fb9121ba9d9645a09e9785a8ab12fbc6543ec2ad
Andrei Neacsu <andnea823@gmail.com>
2021-08-17 12:20:51 -0500

    AAAAAAAAAAAAAA

--- courses/index.php
+++ courses/index.php
@@ -3,7 +3,7 @@
 </head>
     <body>
         <?php include($_SERVER['DOCUMENT_ROOT'] . "/courses/assets/include/courses-header.html") ?>
-        <div class="body">
+        <div id="body">
             <?php include($_SERVER['DOCUMENT_ROOT'] . "/courses/assets/include/courses-nav.html") ?>
             <div class="main">
                 <section id="home">
