9f863be0c67d31dc09ed2b68b6bde7bc6c51a97a
Andrei Neacsu <andnea823@gmail.com>
2021-08-19 11:08:04 -0500

    create lesson 3

---
+++ courses/web-dev/lesson03.php
@@ -0,0 +1,31 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/courses/assets/include/courses-head.html") ?>
+<title>Text Editors</title>
+</head>
+    <body>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . "/courses/assets/include/courses-header.html") ?>
+        <div id="body">
+            <?php include($_SERVER['DOCUMENT_ROOT'] . "/courses/assets/include/courses-nav.html") ?>
+            <div id="main">
+                <section id="project-structure">
+                    <h2 class="title">Project Structure</h2>
+                    <div class="content">
+                        
+                    </div>
+                </section>
+            </div>
+            <div id="footer">
+                <section>
+                    <ul id="lesson-nav">
+                        <li class="previous">
+                            <a href="/courses/web-dev/lesson01.php">&lt; Page 1: HTML Introduction</a>
+                        </li>
+                        <li class="next">
+                            <a href="/courses/web-dev/lesson03.php">Page 3: Basic Website ></a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+        </div>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . "/courses/assets/include/scripts.html") ?>
+    </body>
+</html>
