2445c67c33a3d044c8bdac69ba5d920fa2b2d5df
Andrei Neacsu <andnea823@gmail.com>
2021-08-24 22:40:15 -0500

    delete blog ;-;

--- blog/include/dbh.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "anon", "anon", "biolink");
-    
-    // kill if there is an error
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
