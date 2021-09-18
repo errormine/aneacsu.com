0e1ae8ea7c73f2d1b796e33bc865d4f1e5a96526
Andrei Neacsu <andnea823@gmail.com>
2021-09-18 15:32:16 -0500

    set up 404 page

---
+++ 404.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="shortcut icon" href="/favicon.png">
+        <link rel="stylesheet" href="/style/reset.css">
+        <link rel="stylesheet" href="/style/style.css">
+        <title>404 Not Found</title>
+        <style>
+            html {
+                height: 100%;
+                display: flex;
+                align-items: center;
+                height: 100%;
+            }
+
+            body {
+                width: 40%;
+            }
+        </style>
+    </head>
+    <body> 
+        <main>
+            <section id="websites">
+                <h2 class="title">ERROR 404 PAGE NOT FOUND</h2>
+                <div class="content" style="text-align: center;">
+                    <p style="font-style: italic;">
+                        <?php 
+                        $response = file_get_contents('http://api.quotable.io/random'); 
+                        $response = json_decode($response);
+                        echo "\"" . $response->{'content'} . "\"";
+                        ?>
+                    </p>
+                    <br>
+                    <a href="/">
+                        <input type="button" value="RETURN HOME">
+                    </a>
+                </div>
+            </section>
+        </main>
+    </body>
+</html>
