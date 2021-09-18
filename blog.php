2b163740f3e818ee338f3e9ed91562113229e8f5
Andrei Neacsu <andnea823@gmail.com>
2021-09-18 12:12:35 -0500

    still deciding how to host the blog

---
+++ blog.php
@@ -0,0 +1,15 @@
+<?php include("include/head.html")?>
+    <body> 
+        <?php include("include/header.html")?>
+        <main>
+            <section id="websites">
+                
+                <h2 class="title">!!!</h2>
+                <div class="content">
+                    <p>This page is still under construction please check back soon!</p>
+                </div>
+            </section>
+        </main>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . "/include/footer.html") ?>
+    </body>
+</html>
