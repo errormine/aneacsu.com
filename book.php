eeac902ab4009ee81417fa15cfd2985e7765b7d7
Andrei Neacsu <andnea823@gmail.com>
2021-09-20 18:44:35 -0500

    remove book widget

--- book.php
+++
@@ -1,5 +0,0 @@
-<?php
-$content = file_get_contents('https://openlibrary.org/books/' . $_GET['id'] . '/-/widget');
-$content = str_replace('//openlibrary.org/static/build/page-book-widget.css', '/style/book-widget.css', $content);
-
-echo $content;
