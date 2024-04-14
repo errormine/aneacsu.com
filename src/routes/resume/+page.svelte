ddb434e7d2b9b47481573742ea8d24eabe3ec895
Andrei Neacsu <andnea823@gmail.com>
2024-04-14 13:49:50 -0500

    Fill in resume page with placeholder

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -2,3 +2,8 @@
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
+
+<section>
+    <h2>Under construction!</h2>
+    <p>I will put something here eventually.</p>
+</section>
