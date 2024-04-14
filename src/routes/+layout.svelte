ff9e0466f2ca9432ec2721fd89d91a69cb754217
Andrei Neacsu <andnea823@gmail.com>
2024-04-14 14:24:14 -0500

    Move header outside of main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,31 +5,31 @@
     export let data;
 </script>
 
+<header>
+    <h1>
+        <a href="/">Andrei Neacsu</a>
+    </h1>
+    <nav>
+        <ul>
+            <li>
+                <a href="/resume">Resume</a>
+            </li>
+            <li>
+                <a href="/projects">Projects</a>
+            </li>
+            <li>
+                <a href="/gallery">Gallery</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/blog">Blog</a>
+            </li>
+        </ul>
+    </nav>
+</header>
 <main class="content-grid">
-    <header>
-        <h1 class="full-width">
-            <a href="/">Andrei Neacsu</a>
-        </h1>
-        <nav>
-            <ul>
-                <li>
-                    <a href="/resume">Resume</a>
-                </li>
-                <li>
-                    <a href="/projects">Projects</a>
-                </li>
-                <li>
-                    <a href="/gallery">Gallery</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/blog">Blog</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
     <slot/>
     <footer>
         Created with love for the web.
