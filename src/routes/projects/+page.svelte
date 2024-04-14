da3a13f490905d1c27c18a46816f847c78fabfaa
Andrei Neacsu <andnea823@gmail.com>
2024-04-14 13:46:28 -0500

    Add projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,3 +2,42 @@
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
+
+<section>
+    <h2>Projects</h2>
+    <p>Various projects which I've worked on over the years. Some are finished, some are not.</p>
+    <ol>
+        <li>
+            <strong class="float-right">2024</strong> <a href="https://github.com/prycaustic/plica-garden">Plica Garden</a>
+            <p>VERY simple web front-end for notes / images / other media. Made using Python Flask and vanilla JS.</p>
+        </li>
+        <li>
+            <strong class="float-right">2023</strong> <a href="https://github.com/SlopCrew/SlopCrew">SlopCrew</a>
+            <p>It's a Bomb Rush Cyberfunk multiplayer mod. Skate around with your friends. Originally created by <a href="https://notnite.com/">notnite</a></p>
+        </li>
+        <li>
+            <strong class="float-right">2023</strong><a href="https://errormine.itch.io/the-library-of-babel">The Library of Babel</a>
+            <p>A small puzzle game inspired by the short story by Jorge Luis Borges. Explore the halls of the Universe and solve puzzles that lead to ???</p>
+        </li>
+        <li>
+            <strong class="float-right">2023</strong><a href="https://errormine.itch.io/lunar-lander">Lunar Lander</a>
+            <p>A very simple game made in a few days where you have to land a space craft on the moon.</p>
+        </li>
+        <li>
+            <strong class="float-right">2023</strong><a href="https://errormine.itch.io/creoterra">Creoterra</a>
+            <p>Bring life to an empty planet. Very basic sandbox game which grows as you play it. This was created for the first ever Experimental Game Jam "Grid". I wanted to make something very simple that would develop around your actions. It's a bit unfinished since I ran out of time, but I'm hoping to keep learning and make more!</p>
+        </li>
+        <li>
+            <strong class="float-right">2023</strong><a href="https://errormine.itch.io/boing">Boing!</a>
+            <p>Jump on a trampoline for as long as you can. This is a crappy clone of the WarioWare mini-game of the same name. It's the first thing I created using Godot 3.5, and I think it's pretty cute.</p>
+        </li>
+        <li>
+            <strong class="float-right">2021</strong><a href="https://errormine.net/">errormine.net</a>
+            <p>My own little page on Neocities! I discovered this place while trying to solve some silly ARG. I've met some great friends on there since then.</p>
+        </li>
+        <li>
+            <strong class="float-right">2019</strong> <a href="https://github.com/prycaustic/bent-bot">bent-bot</a>
+            <p>A really terrible Discord bot written with Java which I made in high school. It is mildly embarassing.</p>
+        </li>
+    </ol>
+</section>
