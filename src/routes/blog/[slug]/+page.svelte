4514006e506f8c4a727ccdf90758d668755ec8c9
Andrei Neacsu <andnea823@gmail.com>
2024-04-14 22:35:19 -0500

    Add title to blog post page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,6 +3,10 @@
     export let data;
 </script>
 
+<svelte:head>
+    <link rel="stylesheet" href="/css/prism-cb.css">
+    <title>{data.metadata.title}</title>
+</svelte:head>
 <article>
     <h2 id="top">{data.metadata.title}</h2>
     <svelte:component this={data.content} />
