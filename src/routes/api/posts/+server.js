345eb46d0f76b229e78565381364bfc446a741e1
Andrei Neacsu <andnea823@gmail.com>
2024-04-15 13:06:41 -0500

    Ignore unpublished posts

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -5,28 +5,27 @@ export async function GET() {
     const postFiles = import.meta.glob('/src/routes/blog/_posts/*.md');
     const iPostFiles = Object.entries(postFiles);
 
-    const allPosts = await Promise.all(
-        iPostFiles.map(async ([path, resolver]) => {
-            let { metadata } = await resolver();
-            if (metadata.title === undefined) return;
+    const allPosts = [];
+    for (const [path, resolver] of iPostFiles) {
+        let { metadata } = await resolver();
 
-            const filename = path.split('/').pop();
-            const slug = filename.replace('.md', '');
-            const date = filename.split('-').slice(0, 3).join('-');
-            const dateLocal = new Date(date).toLocaleDateString(undefined, { timeZone: 'UTC', day: 'numeric', month: 'long', year: 'numeric' });
-            
-            metadata = {
-                ...metadata,
-                date,
-                dateLocal,
-            };
+        const filename = path.split('/').pop();
+        const slug = filename.replace('.md', '');
+        const date = filename.split('-').slice(0, 3).join('-');
+        const dateLocal = new Date(date).toLocaleDateString(undefined, { timeZone: 'UTC', day: 'numeric', month: 'long', year: 'numeric' });
+        
+        metadata = {
+            ...metadata,
+            date,
+            dateLocal,
+        };
 
-            return {
-                slug,
-                metadata,
-            };
-        })
-    );
+        if (metadata.published == false) continue;
+        allPosts.push({
+            slug,
+            metadata,
+        });
+    }
 
     const sortedPosts = allPosts.sort((a, b) => {
         return new Date(b.metadata.date) - new Date(a.metadata.date);
