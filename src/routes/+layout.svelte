64d1f1c419111066923eab8ee1af2f118411903e
Andrei Neacsu <andnea823@gmail.com>
2024-04-15 12:14:28 -0500

    Move the footer inside the content grid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,30 +22,30 @@
         <Breadcrumb {routes} />
     </header>
     <slot/>
-</main>
-<footer>
-    <header>
-        <p>Created with love for the web.</p>
-        <section class="contact">
-            <Bubble href="mailto:andrei@aneacsu.com">Email</Bubble>
-            <Bubble href="https://infosec.exchange/@errormine">Mastodon</Bubble>
-            <Bubble href="https://github.com/prycaustic">GitHub</Bubble>
+    <footer>
+        <header>
+            <p>Created with love for the web.</p>
+            <section class="contact">
+                <Bubble href="mailto:andrei@aneacsu.com">Email</Bubble>
+                <Bubble href="https://infosec.exchange/@errormine">Mastodon</Bubble>
+                <Bubble href="https://github.com/prycaustic">GitHub</Bubble>
+            </section>
+        </header>
+        <section class="align-right flex-center">
+            <p>This website is <a href="https://github.com/prycaustic/aneacsu.com">open source</a></p>
+            <p>It was created using <a href="https://kit.svelte.dev/">SvelteKit</a> and <a href="https://www.agnosticui.com/">Agnostic UI</a></p>
+            <p>All of the code for this website is in the public domain</p>
         </section>
-    </header>
-    <section class="align-right flex-center">
-        <p>This website is <a href="https://github.com/prycaustic/aneacsu.com">open source</a></p>
-        <p>It was created using <a href="https://kit.svelte.dev/">SvelteKit</a> and <a href="https://www.agnosticui.com/">Agnostic UI</a></p>
-        <p>All of the code for this website is in the public domain</p>
-    </section>
-    <section>
-        <img src="/img/kot.png" alt="A cat meditating">
-    </section>
-    <section class="flex-center">
-        <p>Add my blog to your <a href="/blog/rss.xml">RSS Feed</a></p>
-        <p>Bored and confused? Read a <a href="https://en.wikipedia.org/wiki/Special:Random">random Wikipedia article</a></p>
-        <p>&ldquo;<i>Round the Calendar in Portugal&rdquo;</i> courtesy of <a href="https://www.flickr.com/photos/britishlibrary/11114321176/in/album-72157672074712488/">The British Library</a></p>
-    </section>
-</footer>
+        <section>
+            <img src="/img/kot.png" alt="A cat meditating">
+        </section>
+        <section class="flex-center">
+            <p>Add my blog to your <a href="/blog/rss.xml">RSS Feed</a></p>
+            <p>Bored and confused? Read a <a href="https://en.wikipedia.org/wiki/Special:Random">random Wikipedia article</a></p>
+            <p>&ldquo;<i>Round the Calendar in Portugal&rdquo;</i> courtesy of <a href="https://www.flickr.com/photos/britishlibrary/11114321176/in/album-72157672074712488/">The British Library</a></p>
+        </section>
+    </footer>
+</main>
 
 <style>
     h1 {
@@ -56,7 +56,7 @@
         display: grid;
         grid-template-columns: 1fr 10rem 1fr;
         gap: 1rem;
-        padding: 2rem;
+        padding: 2rem 0;
     }
 
     footer > header {
