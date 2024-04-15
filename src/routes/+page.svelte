07c010cc794b6fb646f08c4db1a46879b294c7df
Andrei Neacsu <andnea823@gmail.com>
2024-04-14 23:34:17 -0500

    Make contact section into little pill buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,21 @@
     <p>I'm a student in Chicago, IL. I'm also a developer, amateur photographer, and artist. I'm currently interested in video game development, and would love to share some games with you.</p>
     <p>Honestly, I have no clue what I'm doing!</p>
 </section>
+<section>
+    <h2>Contact</h2>
+    <p>Feel free to reach out to me here:</p>
+    <ul class="bubbles">
+        <li>
+            <a href="mailto:andrei@aneacsu.com">Email</a>
+        </li>
+        <li>
+            <a href="https://github.com/prycaustic">GitHub</a>
+        </li>
+        <li>
+            <a rel="me" href="https://infosec.exchange/@errormine">Mastodon</a>
+        </li>
+    </ul>
+</section>
 <section class="links-wrapper">
     <section class="links">
         <h2>Links</h2>
@@ -24,17 +39,6 @@
             </li>
             <li>
                 <a href="/resume">Resume</a>
-            </li>
-        </ul>
-    </section>
-    <section class="social">
-        <h2>Social</h2>
-        <ul>
-            <li>
-                <a href="https://github.com/prycaustic">GitHub</a>
-            </li>
-            <li>
-                <a rel="me" href="https://infosec.exchange/@errormine">Mastodon</a>
             </li>
         </ul>
     </section>
@@ -65,5 +69,27 @@
         gap: 0.5rem;
         list-style-type: none;
         padding: 0;
+    }
+
+    .bubbles {
+        list-style-type: none;
+        display: flex;
+        gap: 1rem;
+    }
+
+    .bubbles li a {
+        position: relative;
+        background: var(--gradient-primary);
+        color: white;
+        border: 1px solid var(--clr-primary-dark);
+        padding: 0.2rem 1rem;
+        border-radius: 100px;
+        top: 0;
+        transition: top 200ms;
+    }
+
+    .bubbles li a:hover {
+        text-decoration: none;
+        top: -0.25rem;
     }
 </style>
