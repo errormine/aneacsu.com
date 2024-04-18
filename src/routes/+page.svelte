081eb96606a8637726f02e53cd8fb279b367675e
Andrei Neacsu <andnea823@gmail.com>
2024-04-18 13:09:53 -0500

    Reorganize home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,20 +15,6 @@
     <p>I'm a student in Chicago, IL. I'm also a developer, amateur photographer, and artist. I'm currently interested in video game development, and would love to share some games with you. Honestly, I have no clue what I'm doing!</p>
 </section>
 <section>
-    <h2>Blog</h2>
-    <ul class="blog-posts">
-        {#each data.posts.slice(0, 5) as post}
-            <li class="flex space-between align-center">
-                <a href={"/blog/"+post.slug}>{post.metadata.title}</a>
-                <span>{post.metadata.date}</span>
-            </li>
-        {/each}
-        <li>
-            <a href="/blog" class="float-right">Previous posts &rarr;</a>
-        </li>
-    </ul>
-</section>
-<section>
     <h2>Projects</h2>
     <p>Various projects which I've worked on over the years. Some are finished, some are not.</p>
     <ol>
@@ -66,7 +52,20 @@
         </li>
     </ol>
 </section>
-
+<section>
+    <h2>Blog</h2>
+    <ul class="blog-posts">
+        {#each data.posts.slice(0, 5) as post}
+            <li class="flex space-between align-center">
+                <a href={"/blog/"+post.slug}>{post.metadata.title}</a>
+                <span>{post.metadata.date}</span>
+            </li>
+        {/each}
+        <li>
+            <a href="/blog" class="float-right">Previous posts &rarr;</a>
+        </li>
+    </ul>
+</section>
 
 <style>
     .blog-posts {
